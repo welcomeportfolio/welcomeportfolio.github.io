ffcb8633e4f877c923f511000b8fcef46def74c1
welcomeportfolio <112360626+welcomeportfolio@users.noreply.github.com>
2022-08-29 11:01:55 +0900

    Add files via upload

---
+++ contact/js/5-5.js
@@ -0,0 +1,17 @@
+particlesJS("particles-js", {"particles":
+{"number":{"value":433,"density":
+{"enable":true,"value_area":6092.965831288987}},"color":
+{"value":"#fff"},"shape":{"type":"circle","stroke":
+{"width":0,"color":"#000000"},"polygon":{"nb_sides":7},"image":
+{"src":"img/github.svg","width":100,"height":100}},"opacity":
+{"value":0.3314420094855545,"random":true,"anim":
+{"enable":false,"speed":1,"opacity_min":0.1,"sync":false}},"size":{"value":24.05118091298284,"random":true,"anim":
+{"enable":false,"speed":40,"size_min":0.1,"sync":false}},"line_linked":
+{"enable":false,"distance":336.7165327817598,"color":"#FFFFFF","opacity":0.4,"width":1.9240944730386271},"move":
+{"enable":true,"speed":6,"direction":"top","random":false,"straight":false,"out_mode":"out","bounce":false,"attract":
+{"enable":false,"rotateX":721.5354273894853,"rotateY":1200}}},"interactivity":
+{"detect_on":"canvas","events":{"onhover":{"enable":true,"mode":"bubble"},"onclick":{"enable":false,"mode":"repulse"},"resize":true},"modes":
+{"grab":{"distance":400,"line_linked":{"opacity":0.5}},"bubble":
+{"distance":400,"size":4,"duration":0.3,"opacity":1,"speed":3},"repulse":
+{"distance":200,"duration":0.4},"push":{"particles_nb":4},"remove":
+{"particles_nb":2}}},"retina_detect":true});
